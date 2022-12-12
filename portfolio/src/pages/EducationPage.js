9e7b7c20878c6a86696c14d975a7aa1b21e4392c
Jayden-58 <jayden.kellar@gmail.com>
2022-12-11 19:09:10 -0800

    some more changes

--- portfolio/src/pages/EducationPage.js
+++ portfolio/src/pages/EducationPage.js
@@ -1,9 +1,21 @@
-import {Container} from 'reactstrap';
+import {Container, Row, Col} from 'reactstrap';
 
 const EducationPage = () => {
     return(
-        <Container>
-            <p>This is the education page</p>
+        <Container fluid style={{background: 'radial-gradient(circle, rgba(69,27,33,1) 25%, rgba(28,30,31,1) 75%)'}}>
+            <Row>
+                <Col className='col-12'>
+                    <h1 style={{marginTop: 0, color: 'white'}}>This is the Education Page</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col className='md-6' >
+                    <p style = {{marginBottom: 0, paddingBottom: 5, color: 'white'}}>wazzzzzzupppp</p>
+                </Col>
+                <Col className='md-6' >
+                    <p style = {{marginBottom: 0, paddingBottom: 5, color: 'white'}}>wazzuuuupppp</p>
+                </Col>
+            </Row>
         </Container>
     )
 };
--- portfolio/src/pages/HomePage.js
+++ portfolio/src/pages/HomePage.js
@@ -1,16 +1,16 @@
 import {Container, Row, Col} from 'reactstrap';
 import jaydenImage from '../app/assets/jayden_image.png';
-
+import '../App.css';
 const imgSize = {
-    maxWidth: 900
+    maxWidth: 400
 }
 
 const HomePage = () => {
     return(
-        <Container style={{background: 'radial-gradient(circle, rgba(69,27,33,1) 25%, rgba(28,30,31,1) 75%)'}}>
+        <Container fluid style={{background: 'radial-gradient(circle, rgba(69,27,33,1) 25%, rgba(28,30,31,1) 75%)'}}>
             <Row>
                 <Col>
-                    <h1 style={{marginTop: 0}}>This is the homepage</h1>
+                    <h1 style={{color: 'green',marginTop: 0,fontFamily: 'Source Code Pro'}}>Hello World!</h1>
                 </Col>
             </Row>
             <Row>
@@ -19,11 +19,19 @@ const HomePage = () => {
                 </Col>
             </Row>
             <Row>
-                <Col className='md-6' >
-                    <p style = {{marginBottom: 0, paddingBottom: 5}}>wazzuuuupppp</p>
+                <Col className='md-4 mt-1' >
+                    <h1 style = {{color: 'white', marginBottom: 0, paddingBottom: 5}}>My mission:</h1>
                 </Col>
-                <Col className='md-6' >
-                    <p style = {{marginBottom: 0, paddingBottom: 5}}>wazzuuuupppp</p>
+                <Col className='md-8 mt-2' >
+                    <p style = {{color: 'white', marginBottom: 0, paddingBottom: 5}}>blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col className='md-4 mt-1' >
+                    <h1 style = {{color: 'white', marginBottom: 0, paddingBottom: 5}}>My mission:</h1>
+                </Col>
+                <Col className='md-8 mt-2' >
+                    <p style = {{color: 'white', marginBottom: 0, paddingBottom: 5}}>blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
                 </Col>
             </Row>
         </Container>
