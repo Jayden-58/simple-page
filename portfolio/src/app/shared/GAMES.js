1496e795037da9ad736b4146a06e4a84a444e184
Jayden-58 <jayden.kellar@gmail.com>
2022-12-14 15:05:55 -0800

    games page now functional

--- portfolio/src/app/shared/GAMES.js
+++ portfolio/src/app/shared/GAMES.js
@@ -8,20 +8,24 @@ export const GAMES = [
         id: 0,
         name: 'Supply Run',
         image: supplyRun,
+        url: 'https://jayden58.itch.io/supply-run-unity3d-fps-prototype',
     },
     {
         id: 1,
         name: 'Plane Rail Shooter',
         image: PlaneRailShooter,
+        url: 'https://jayden58.itch.io/plane-rail-shooter-1-min-experience'
     },
     {
         id: 2,
         name: 'Zombie Tower Defense',
         image: ZombieDefence,
+        url: 'https://jayden58.itch.io/zombie-defense-prototype-2'
     },
     {
         id: 3,
         name: 'Shark Swimmer',
         image: SharkSwim,
+        url: 'https://jayden-58.github.io/Shark-Swim-Unity3D-/'
     },
 ]
--- portfolio/src/components/GameCard.js
+++ portfolio/src/components/GameCard.js
@@ -1,13 +1,14 @@
-import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
+import {Card, CardImg, CardText, CardBody, CardTitle, Button} from 'reactstrap';
 
 const GameCard = ({game}) => {
-    const {name, image} = game;
+    const {name, image, url} = game;
 
     return (
-        <Card>
+        <Card style={{ backgroundColor: 'rgba(28,30,31,1)', boxShadow: '4px 3px 2px rgb(13, 13, 13)'}}>
             <CardImg src={image} alt={name} />
             <CardBody>
-                <CardTitle>{name}</CardTitle>
+                <CardTitle style={{fontFamily: 'Bebas Neue'}} className='text-light h1'>{name}</CardTitle>{/*fix this later for responsiveness */}
+                <Button style={{backgroundColor: '#722F37'}} href={url}>Play!</Button>
             </CardBody>
         </Card>
     )
